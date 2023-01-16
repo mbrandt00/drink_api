8c4f6593b9ca5b0dff7203942dde87dca14d042b
Michael Brandt <msbrandt00@gmail.com>
2023-01-15 23:45:35 -0700

    Create users

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,14 @@
 class Api::V1::UsersController < ApplicationController
   def create
-    binding.pry
+    user = User.find_or_create_by(email: params[:email]) do |user|
+      user.first_name = params[:given_name]
+      user.last_name = params[:family_name]
+    end
+    if user
+      render json: {user: user, message: 'Logged in Successfully'}, status: :ok
+    else
+      render :nothing => true, :status => :unprocessable_entity
+    end
   end
 
   def update
