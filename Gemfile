580fe036e956ac9029f27a04b2d6585f93141cb9
Michael Brandt <msbrandt00@gmail.com>
2023-01-16 11:47:39 -0700

    Setup loggin in logging out

--- Gemfile
+++ Gemfile
@@ -4,7 +4,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby '3.0.0'
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
-gem 'rails', '~> 7.0.4'
+gem 'rails', '~> 6.1'
 
 # Use postgresql as the database for Active Record
 gem 'pg', '~> 1.1'
@@ -34,8 +34,8 @@ gem 'bootsnap', require: false
 # gem "image_processing", "~> 1.2"
 
 # Use Rack CORS for handling Cross-Origin Resource Sharing (CORS), making cross-origin AJAX possible
-gem "rack-cors"
 gem 'faker'
+gem 'rack-cors'
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,21 +1,39 @@
 class Api::V1::UsersController < ApplicationController
+  before_action :find_user, except: %i[create logout]
   def create
     user = User.find_or_create_by(email: params[:email]) do |user|
       user.first_name = params[:given_name]
       user.last_name = params[:family_name]
     end
     if user
-      render json: {user: user, message: 'Logged in Successfully'}, status: :ok
+      session[:user_id] = user.id
+      render json: { user: user, logged_in: true, status: :created }
     else
-      render :nothing => true, :status => :unprocessable_entity
+      render json: { status: 401 }
     end
   end
 
   def update
-    binding.pry
+    # todo
+  end
+
+  def logout
+    session[:user_id] = nil
+    render json: {
+      logged_in: false
+    }
   end
 
   def destroy
-    binding.pry
+    # logout
+    return unless @user.delete
+
+    render json: { message: 'User deleted' }, status: :no_content
+  end
+
+  private
+
+  def find_user
+    @user = User.find(params[:id])
   end
 end
